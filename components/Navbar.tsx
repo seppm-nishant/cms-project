93164105f162e041b6e4321103a48ca675819cb9
shobhit <shobhitmishra2002@gmail.com>
2022-12-15 21:36:48 +0530

    added the ui of home page

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="py-6 px-4 shadow-lg">
+      <h2 className="text-3xl text-gray-600 cursor-pointer">Logo</h2>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,30 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <div className="text-2xl text-center">hello world</div>
+      <main className="pt-8">
+        <div className="flex justify-evenly pt-28 items-center align-middle">
+          <div className="max-w-xl">
+            <h2 className="text-4xl pb-4 text-gray-600">
+              Main heading comes here...
+            </h2>
+            <p className="text-gray-500 pb-6 text-sm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              impedit at quisquam ducimus nisi perferendis distinctio blanditiis
+              fugiat ad, atque nesciunt culpa mollitia necessitatibus, rem
+              deleniti maxime possimus adipisci consequatur.
+            </p>
+
+            <button className="text-xl text-white px-8 py-2 rounded-md bg-blue-500 hover:bg-blue-600">
+              Enter
+            </button>
+          </div>
+
+          <div>
+            <div className="w-[400px] h-[400px] bg-gray-400 rounded-md text-4xl flex justify-center align-middle items-center text-gray-600">
+              IMAGE
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
